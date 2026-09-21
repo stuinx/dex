4fe8a8b8173c6a8a9d7e523dd4189ac6298bd391
stuinx <83058107+stuinx@users.noreply.github.com>
2026-09-21 08:54:41 +0800

    Add RproxyS mappings, HAProxy rules, and extra inbounds

--- resource/client/ui-web/act/RproxyCsave.php
+++ resource/client/ui-web/act/RproxyCsave.php
@@ -1,17 +1,16 @@
 <?php require_once('../auth.php'); ?>
 <?php if (isset($auth) && $auth) {?>
 <?php
-$RproxyCtunnelAddress = $_GET['RproxyCtunnelAddress'];
-$RproxyCtunnelUUID = $_GET['RproxyCtunnelUUID'];
-$RproxyCoutStatus = $_GET['RproxyCoutStatus'];
-$RproxyCmappingStatus = $_GET['RproxyCmappingStatus'];
-$RproxyC1List = $_GET['RproxyC1List'];
+$RproxyCtunnelAddress = $_GET['RproxyCtunnelAddress'] ?? '';
+$RproxyCtunnelUUID = $_GET['RproxyCtunnelUUID'] ?? '';
+$RproxyCmappingStatus = $_GET['RproxyCmappingStatus'] ?? '';
+$RproxyC1List = $_GET['RproxyC1List'] ?? array();
 
 $data = json_decode(file_get_contents('/opt/de_GWD/0conf'), true);
 $data['FORWARD']['Rproxy']['client'] = array();
 $data['FORWARD']['Rproxy']['client']['status'] = "on";
-if ($RproxyCoutStatus === block) $data['FORWARD']['Rproxy']['client']['outStatus'] = "on";
-if ($RproxyCmappingStatus === block) $data['FORWARD']['Rproxy']['client']['mappingStatus'] = "on";
+$data['FORWARD']['Rproxy']['client']['outStatus'] = "off";
+$data['FORWARD']['Rproxy']['client']['mappingStatus'] = ($RproxyCmappingStatus === 'block') ? "on" : "off";
 $data['FORWARD']['Rproxy']['client']['tunnel']['address'] = $RproxyCtunnelAddress;
 $data['FORWARD']['Rproxy']['client']['tunnel']['uuid'] = $RproxyCtunnelUUID;
 $data['FORWARD']['Rproxy']['client']['mapping'] = $RproxyC1List;
--- resource/client/ui-web/act/RproxySsave.php
+++ resource/client/ui-web/act/RproxySsave.php
@@ -1,21 +1,26 @@
 <?php require_once('../auth.php'); ?>
 <?php if (isset($auth) && $auth) {?>
 <?php
-$RproxyStunnelPort = $_GET['RproxyStunnelPort'];
-$RproxyStunnelUUID = $_GET['RproxyStunnelUUID'];
-$RproxySinStatus = $_GET['RproxySinStatus'];
-$RproxyS0uuidList = $_GET['RproxyS0uuidList'];
-$RproxySmappingStatus = $_GET['RproxySmappingStatus'];
-$RproxyS1List = $_GET['RproxyS1List'];
+$RproxyStunnelPort = $_GET['RproxyStunnelPort'] ?? '';
+$RproxyStunnelUUID = $_GET['RproxyStunnelUUID'] ?? '';
+$RproxySmappingStatus = $_GET['RproxySmappingStatus'] ?? '';
+$RproxyS1List = $_GET['RproxyS1List'] ?? array();
+if (!preg_match('/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/', $RproxyStunnelUUID)) {
+  $RproxyUUIDBytes = random_bytes(16);
+  $RproxyUUIDBytes[6] = chr((ord($RproxyUUIDBytes[6]) & 0x0f) | 0x40);
+  $RproxyUUIDBytes[8] = chr((ord($RproxyUUIDBytes[8]) & 0x3f) | 0x80);
+  $RproxyUUIDHex = bin2hex($RproxyUUIDBytes);
+  $RproxyStunnelUUID = substr($RproxyUUIDHex, 0, 8) . '-' . substr($RproxyUUIDHex, 8, 4) . '-' . substr($RproxyUUIDHex, 12, 4) . '-' . substr($RproxyUUIDHex, 16, 4) . '-' . substr($RproxyUUIDHex, 20, 12);
+}
 
 $data = json_decode(file_get_contents('/opt/de_GWD/0conf'), true);
 $data['FORWARD']['Rproxy']['server'] = array();
 $data['FORWARD']['Rproxy']['server']['status'] = "on";
-if ($RproxySinStatus === block) $data['FORWARD']['Rproxy']['server']['inStatus'] = "on";
-if ($RproxySmappingStatus === block) $data['FORWARD']['Rproxy']['server']['mappingStatus'] = "on";
+$data['FORWARD']['Rproxy']['server']['inStatus'] = "off";
+$data['FORWARD']['Rproxy']['server']['mappingStatus'] = ($RproxySmappingStatus === 'block') ? "on" : "off";
 $data['FORWARD']['Rproxy']['server']['tunnel']['port'] = $RproxyStunnelPort;
 $data['FORWARD']['Rproxy']['server']['tunnel']['uuid'] = $RproxyStunnelUUID;
-$data['FORWARD']['Rproxy']['server']['inUUID'] = $RproxyS0uuidList;
+$data['FORWARD']['Rproxy']['server']['inUUID'] = array();
 $data['FORWARD']['Rproxy']['server']['mapping'] = $RproxyS1List;
 $newJsonString = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 file_put_contents('/opt/de_GWD/0conf', $newJsonString);
